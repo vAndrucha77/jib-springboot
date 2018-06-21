cca31d04a6fe6d54dbe5b14c76af0634f6df4c8a
Mohammed Aboullaite <aboullaite.mohammed@gmail.com>
2018-06-21 18:08:45 +0100

    Initial commit

---
+++ src/main/java/me/aboullaite/jib/gitlabregistry/GitlabRegistryApplication.java
@@ -0,0 +1,12 @@
+package me.aboullaite.jib.gitlabregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlabRegistryApplication.class, args);
+    }
+}
